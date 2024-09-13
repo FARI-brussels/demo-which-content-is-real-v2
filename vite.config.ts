2af21ae9138863d04ced61ecae4207e2d67990fc
m0satron <msebdani@gmail.com>
2024-09-13 09:58:46 +0200

    disable devtools

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(), 
+    // vueDevTools()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
